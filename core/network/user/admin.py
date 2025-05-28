5939eef487d1de864a766611feee030f08e5b36c
Furthermore <wrbyepcr11@gmail.com>
2025-05-29 03:03:24 +0800

    feat(login): add facebook, twitter social login and sign up

--- core/network/user/admin.py
+++ core/network/user/admin.py
@@ -11,4 +11,24 @@ User = get_user_model()
 class CustomUserAdmin(UserAdmin):
     """User admin."""
 
-    ordering = ["email"]
+    model = User
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {"fields": ("is_staff", "is_active", "groups", "user_permissions")},
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "is_staff", "is_active"),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
