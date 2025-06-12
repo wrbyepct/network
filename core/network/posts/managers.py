c98ed74e01c0bdf6edbe1cc3b35f58338031c44d
Furthermore <wrbyepcr11@gmail.com>
2025-06-12 15:31:18 +0800

    feat(profile): Allow user to see their own posts in their profile posts page

--- core/network/posts/managers.py
+++ core/network/posts/managers.py
@@ -6,6 +6,10 @@ from django.db import models
 class PostQuerySet(models.QuerySet):
     """Customized Post queryset."""
 
+    def with_profile(self):
+        """Select related profile."""
+        return self.select_related("user__profile")
+
     def with_media(self):
         """Prefetch with medias."""
         return self.prefetch_related("medias")
@@ -25,3 +29,7 @@ class PostManger(models.Manager):
     def with_media(self):
         """Optimized post search query when querying with 'medias'."""
         return self.get_queryset().with_media()
+
+    def for_list_data(self):
+        """Return optimized posts with medias and profile data."""
+        return self.get_queryset().with_media().with_profile()
