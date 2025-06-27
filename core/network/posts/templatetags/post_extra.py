461584afa3c51e67086231bd46972d2ef97cd246
Furthermore <wrbyepcr11@gmail.com>
2025-06-28 01:39:39 +0800

    feat(comment): Display like button on comments

--- core/network/posts/templatetags/post_extra.py
+++ core/network/posts/templatetags/post_extra.py
@@ -1,13 +1,21 @@
 """Custom template tags for post template."""
 
 from django import template
+from django.utils.timesince import timesince
 
 register = template.Library()
 
 
 @register.filter
-def liked_by_user(post, user):
-    """Check if a post is liked by a user."""
+def liked_by_user(obj, user):
+    """Check if the instance is liked by a user."""
     if not user.is_authenticated:
         return False
-    return post.likes.filter(user=user).exists()
+    return obj.likes.filter(user=user).exists()
+
+
+@register.filter
+def timesince_simple(value):
+    """Return fisrt part of timesince."""
+    result = timesince(value)
+    return result.split(",")[0]
