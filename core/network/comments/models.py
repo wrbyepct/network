3ea80e9a05d3607c436054fab7744e6870e06eb7
Furthermore <wrbyepcr11@gmail.com>
2025-06-28 11:34:08 +0800

    feat(comment): Add CommentLike model and refactor like count logic into mixin

--- core/network/comments/models.py
+++ core/network/comments/models.py
@@ -4,12 +4,12 @@ from django.conf import settings
 from django.core.validators import MinLengthValidator
 from django.db import models
 
-from network.common.mixins import ProfileInfoMixin
+from network.common.mixins import LikeCountMixin, ProfileInfoMixin
 from network.common.models import TimestampedModel
 from network.posts.models import Post
 
 
-class Comment(ProfileInfoMixin, TimestampedModel):
+class Comment(LikeCountMixin, ProfileInfoMixin, TimestampedModel):
     """Commnet model associated with a Post."""
 
     content = models.TextField(validators=[MinLengthValidator(1)])
@@ -29,3 +29,19 @@ class Comment(ProfileInfoMixin, TimestampedModel):
     def __str__(self) -> str:
         """Return string: 'User: {self.user.id} comment on Post: {self.post.id}'."""
         return f"User: {self.user.id} comment on Post: {self.post.id}"
+
+
+class CommentLike(TimestampedModel):
+    """Like model for comment."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comment_likes"
+    )
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta(TimestampedModel.Meta):
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "comment"], name="unique_like_per_comment"
+            )
+        ]
