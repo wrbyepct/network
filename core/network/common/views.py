0aac73b56cdf95d6a35c0fc7114163dd49e8ad42
Furthermore <wrbyepcr11@gmail.com>
2025-07-04 15:34:13 +0800

    feat(comment): Make delete comment done partially
    Frontend can immeidately see the common deleted if the delete success

---
+++ core/network/common/views.py
@@ -0,0 +1,12 @@
+"""View for all apps."""
+
+from django.http import HttpResponse
+from django.views.generic import View
+
+
+class EmptyView(View):
+    """View to return empty html body on GET request."""
+
+    def get(self, request, *args, **kwargs):
+        """Return empty request on get."""
+        return HttpResponse()
