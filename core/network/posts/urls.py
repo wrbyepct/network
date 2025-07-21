58ed2b67e3d5c54ca86ca4181ccb869f3ce15f38
Furthermore <wrbyepcr11@gmail.com>
2025-07-21 14:26:27 +0800

    refactor(index): Refactor post list view into home index

--- core/network/posts/urls.py
+++ core/network/posts/urls.py
@@ -6,11 +6,6 @@ from . import views
 
 urlpatterns = [
     path(
-        "all/",
-        views.PostListView.as_view(),
-        name="post_list",
-    ),
-    path(
         "create/",
         views.PostCreateView.as_view(),
         name="post_create",
