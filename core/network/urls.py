98822c2142874422e5abd8f86718325df0f877b9
Furthermore <wrbyepcr11@gmail.com>
2025-09-08 02:38:02 +0800

    feat(account): Restyle reset password modal content & alert message
    Also: 1. Fix account login signup form invalid valid display & action handle 2. Change post dropwdown, action bar bg color

--- core/network/urls.py
+++ core/network/urls.py
@@ -7,6 +7,8 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("accounts/signup/", views.CustomSignupView.as_view(), name="account_signup"),
+    path("accounts/login/", views.CustomLoginView.as_view(), name="account_login"),
     path("accounts/", include("allauth.urls")),
     path("profiles/", include("network.profiles.urls")),
     path("posts/", include("network.posts.urls")),
